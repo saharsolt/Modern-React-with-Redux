304bbfe9ad57d522f4b42ac1b7602d5365265c04
Sahar <sahar.solt@gmail.com>
2022-10-16 22:52:41 +0300

    Building content with JSX

---
+++ 001_-_jsx/src/index.js
@@ -0,0 +1,13 @@
+// 1) Import the React and React DOM libraries
+import React from "react";
+import ReactDOM from "react-dom/client";
+// 2) Get a reference to the div with id root
+const el = document.getElementById("root");
+// 3) Tell React to take control of that element
+const root = ReactDOM.createRoot(el);
+// 4) Create a component
+function App() {
+  return <h1>Hi there!</h1>;
+}
+// 5) Show the component on the screen
+root.render(<App />);
