394e4a94c931b19b30948367721ab1573a804d5f
Sahar <sahar.solt@gmail.com>
2022-10-17 12:52:59 +0300

    Typical component layouts

--- 001_-_jsx/src/index.js
+++ 001_-_jsx/src/index.js
@@ -7,10 +7,14 @@ const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 // 4) Create a component
 function App() {
-  // const date = new Date();
-  // const time = date.toLocaleTimeString();
-  // return <h1>{time}</h1>;
-  return <h1>{new Date().toLocaleTimeString()}</h1>;
+  const name = "Sahar";
+  const age = 33;
+
+  return (
+    <h1>
+      Hi, my name is {name}, and my age is {age}.
+    </h1>
+  );
 }
 // 5) Show the component on the screen
 root.render(<App />);
