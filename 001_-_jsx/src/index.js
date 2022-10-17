a62ece3c7e67e9d2b67f25d86ca25f27ca36794e
Sahar <sahar.solt@gmail.com>
2022-10-17 15:45:53 +0300

    Applying styling in JSx

--- 001_-_jsx/src/index.js
+++ 001_-_jsx/src/index.js
@@ -7,7 +7,10 @@ const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 // 4) Create a component
 function App() {
-  return <textarea autoFocus={true} />;
+  //return <input maxLength={6} />;
+  // return <input style={{ border: "1px solid blue", paddingTop: "5px" }} />;
+  // return <input spellCheck={false} />;
+  return <input spellCheck />;
 }
 // 5) Show the component on the screen
 root.render(<App />);
