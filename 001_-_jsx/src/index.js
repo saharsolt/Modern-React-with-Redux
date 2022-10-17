0e85da13ba957a45ce633e01d4b9fa8e88a04f4c
Sahar <sahar.solt@gmail.com>
2022-10-17 12:30:15 +0300

    Shorthand JS expressions

--- 001_-_jsx/src/index.js
+++ 001_-_jsx/src/index.js
@@ -7,11 +7,10 @@ const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 // 4) Create a component
 function App() {
-  let message = "Bye there!";
-
-  if (Math.random() > 0.5) message = "Hello there!";
-
-  return <h1>{message}</h1>;
+  // const date = new Date();
+  // const time = date.toLocaleTimeString();
+  // return <h1>{time}</h1>;
+  return <h1>{new Date().toLocaleTimeString()}</h1>;
 }
 // 5) Show the component on the screen
 root.render(<App />);
