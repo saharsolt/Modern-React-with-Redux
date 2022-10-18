05b7715965eb95cdbb3e91a33a829998e9e6b13e
Sahar <sahar.solt@gmail.com>
2022-10-18 20:09:22 +0300

    Component nesting

--- 002_-_components/src/index.js
+++ 002_-_components/src/index.js
@@ -1,52 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
+import CommentDetail from "./CommentDetail.js";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.image()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sahar
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 11:30 PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.image()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Masoud
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 11:40 PM</span>
-          </div>
-          <div className="text">Nice!</div>
-        </div>
-      </div>
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src={faker.image.image()} />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Neda
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 11:00 PM</span>
-          </div>
-          <div className="text">Good blog post!</div>
-        </div>
-      </div>
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
     </div>
   );
 };
