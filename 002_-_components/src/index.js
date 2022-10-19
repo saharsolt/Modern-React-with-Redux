4b7a65611065765ba584c63ca9e1add5f79c08d8
Sahar <sahar.solt@gmail.com>
2022-10-19 12:06:21 +0300

    Showing custom children

--- 002_-_components/src/index.js
+++ 002_-_components/src/index.js
@@ -7,25 +7,30 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Sahar"
-        timeAgo="Today at 4:00PM"
-        comment="Well done!"
-        avatar={faker.image.image()}
-      />
-      <CommentDetail
-        author="Neda"
-        timeAgo="Today at 6:40AM"
-        comment="Nice blog post!"
-        avatar={faker.image.image()}
-      />
-      <CommentDetail
-        author="Masoud"
-        timeAgo="Yesterday at 8:15PM"
-        comment="I like the subject"
-        avatar={faker.image.image()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sahar"
+          timeAgo="Today at 4:00PM"
+          comment="Well done!"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Neda"
+          timeAgo="Today at 6:40AM"
+          comment="Nice blog post!"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Masoud"
+          timeAgo="Yesterday at 8:15PM"
+          comment="I like the subject"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
