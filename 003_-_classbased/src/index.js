dcb9fe23a09d023120f06cf28f3dcda6284c0504
Sahar <sahar.solt@gmail.com>
2022-10-20 13:30:58 +0300

    Getting a user physical location

--- 003_-_classbased/src/index.js
+++ 003_-_classbased/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (err) => console.error(err)
+  );
   return <div>Hi there!</div>;
 };
 
