ce9dcf513fa035f03557fa1803f0a2e9e25d8bb6
Sahar <sahar.solt@gmail.com>
2022-10-20 13:24:57 +0300

    Scaffolding the app

---
+++ 003_-_classbased/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+
+const App = () => {
+  return <div>Hi there!</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
