aa672cb22853adeb57233e03630c29d697ea467e
Sahar <sahar.solt@gmail.com>
2022-10-21 19:03:43 +0300

    Refactoring some codes

--- 003_-_classbased/src/SeasonDisplay.js
+++ 003_-_classbased/src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from "react";
 
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach",
+    iconName: "sun",
+  },
+  winter: {
+    text: "Burr, it is chilly!",
+    iconName: "snowflake",
+  },
+};
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? "summer" : "winter";
@@ -11,15 +22,13 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = (props) => {
   // console.log(props.lat);
   const season = getSeason(props.lat, new Date().getMonth());
-  const text =
-    season === "winter" ? "Burr, It is chilly!" : "Lets hit the beach!";
-  const icon = season === "winter" ? "snowflake" : "sun";
+  const { text, iconName } = seasonConfig[season]; //{text,iconName}
 
   return (
     <div>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
       <h1>{text}</h1>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
     </div>
   );
 };
