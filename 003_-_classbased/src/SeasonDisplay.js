46ecd0cc4d3b1a1742ef8fdb223f2f8a88fa2f85
Sahar <sahar.solt@gmail.com>
2022-10-21 18:50:54 +0300

    Showing icons

--- 003_-_classbased/src/SeasonDisplay.js
+++ 003_-_classbased/src/SeasonDisplay.js
@@ -11,8 +11,16 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = (props) => {
   // console.log(props.lat);
   const season = getSeason(props.lat, new Date().getMonth());
-  console.log(season);
+  const text =
+    season === "winter" ? "Burr, It is chilly!" : "Lets hit the beach!";
+  const icon = season === "winter" ? "snowflake" : "sun";
 
-  return <div>Season Display</div>;
+  return (
+    <div>
+      <i className={`${icon} icon`} />
+      <h1>{text}</h1>
+      <i className={`${icon} icon`} />
+    </div>
+  );
 };
 export default SeasonDisplay;
