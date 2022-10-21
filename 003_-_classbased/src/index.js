dd3397c8c44c885c8a617bf0b07b7b297f391261
Sahar <sahar.solt@gmail.com>
2022-10-21 13:01:58 +0300

    Handling errors

--- 003_-_classbased/src/index.js
+++ 003_-_classbased/src/index.js
@@ -6,21 +6,30 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     // This is the only time we do the direct assignment to this.state
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: "" };
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
-        console.log(position);
+        //console.log(position);
         // We called setState
         this.setState({ lat: position.coords.latitude });
       },
-      (err) => console.error(err)
+      (err) => {
+        //console.error(err);
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
   // React says we have to define render
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        Error: {this.state.errorMessage}
+      </div>
+    );
   }
 }
 
