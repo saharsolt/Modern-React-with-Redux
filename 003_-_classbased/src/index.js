7c94efb27b62f0ea8c6c05826317308979462fc9
Sahar <sahar.solt@gmail.com>
2022-10-21 20:29:31 +0300

    Avoiding conditionals in render

--- 003_-_classbased/src/index.js
+++ 003_-_classbased/src/index.js
@@ -16,8 +16,8 @@ class App extends React.Component {
   componentDidUpdate() {
     console.log("My component was just updated - it rerendered!");
   }
-  // React says we have to define render!
-  render() {
+
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
@@ -25,6 +25,10 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
     return <Spinner message="Please accept the location request!" />;
+  }
+  // React says we have to define render!
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
