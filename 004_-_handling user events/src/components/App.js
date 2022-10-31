c7d180ea8d8f656615c0041a71774ea71d03bbc1
Sahar <sahar.solt@gmail.com>
2022-10-31 10:57:41 +0200

    Handling request with async/ await syntax

--- 004_-_handling user events/src/components/App.js
+++ 004_-_handling user events/src/components/App.js
@@ -3,21 +3,17 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
+  async onSearchSubmit(term) {
     // console.log(term);
-    axios
-      .get("https://api.unsplash.com/search/photos", {
-        params: {
-          query: term,
-        },
-        headers: {
-          Authorization:
-            "Client-ID 3xYqZrig9pjEGrTGqHm9_Polwfdukq4X2e7IEpoXr38",
-        },
-      })
-      .then((response) => {
-        console.log(response.data.results);
-      });
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: {
+        query: term,
+      },
+      headers: {
+        Authorization: "Client-ID 3xYqZrig9pjEGrTGqHm9_Polwfdukq4X2e7IEpoXr38",
+      },
+    });
+    console.log(response.data.results);
   }
 
   render() {
