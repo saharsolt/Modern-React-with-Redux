917b70c890121d84385f422386ceef75103069e1
Sahar <sahar.solt@gmail.com>
2022-11-26 20:09:18 +0200

    Iframe

---
+++ 006_-_video/src/components/VideoDetail.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const VideoDetail = ({ video }) => {
+  if (!video) {
+    return <div>Loading...</div>;
+  }
+
+  const videoSrc = `https://youtube.com/embed/${video.id.videoId}`;
+  return (
+    <div>
+      <div className="ui embed">
+        <iframe src={videoSrc} />
+      </div>
+      <div className="ui segment">
+        <div className="header">{video.snippet.title}</div>
+        <h4>{video.snippet.description}</h4>
+      </div>
+    </div>
+  );
+};
+export default VideoDetail;
