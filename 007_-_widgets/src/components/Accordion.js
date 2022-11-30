6c9aa58e70f58eff9f3dfbb4f7c6bad207f399f5
Sahar <sahar.solt@gmail.com>
2022-11-30 23:24:06 +0200

    Render items

--- 007_-_widgets/src/components/Accordion.js
+++ 007_-_widgets/src/components/Accordion.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Accordion = ({ items }) => {
-  return <div>{items.length}</div>;
+  const onTitleClick = (index) => {
+    console.log("Title clicked ", index);
+  };
+
+  const renderItem = items.map((item, index) => {
+    return (
+      <React.Fragment key={item.title}>
+        <div className="title active" onClick={() => onTitleClick(index)}>
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
+
+  return <div className="ui styled accordion">{renderItem}</div>;
 };
 
 export default Accordion;
