ae343d7ec8f7bc4e8e671d058be8de498b6f9403
Sahar <sahar.solt@gmail.com>
2022-12-05 23:04:43 +0200

    Executing the request from useEffect

--- 007_-_widgets/src/components/Search.js
+++ 007_-_widgets/src/components/Search.js
@@ -1,11 +1,23 @@
 import React, { useState, useEffect } from "react";
-
+import axios from "axios";
 const Search = () => {
-  const [term, setTerm] = useState("");
-
-  console.log("I run with every render");
+  const [term, setTerm] = useState("Programming");
+  const [results, setResults] = useState([]);
+  console.log(results);
   useEffect(() => {
-    console.log("I run after every render and at initial render change");
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    };
+    search();
   }, [term]);
 
   return (
