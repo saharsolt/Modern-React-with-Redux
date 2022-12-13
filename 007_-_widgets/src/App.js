7dbe9d9aa83e8709180a4da3cc5cbde64cd46cae
Sahar <sahar.solt@gmail.com>
2022-12-13 23:51:42 +0200

    Selection state

--- 007_-_widgets/src/App.js
+++ 007_-_widgets/src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
+import Dropdown from "./components/Dropdown";
 import Search from "./components/Search";
 
 const items = [
@@ -17,10 +18,21 @@ const items = [
   },
 ];
 
+const options = [
+  { label: "The color Red", value: "red" },
+  { label: "The color Green", value: "green" },
+  { label: "The shade of Blue", value: "blue" },
+];
+
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
-      <Search />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };
