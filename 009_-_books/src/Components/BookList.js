2e4f715c77e416df8cfe79357c6c57323c2e7f56
Sahar <sahar.solt@gmail.com>
2023-03-08 16:51:35 +0200

    Context system

--- 009_-_books/src/Components/BookList.js
+++ 009_-_books/src/Components/BookList.js
@@ -1,12 +1,21 @@
 import BookShow from "./BookShow";
+import BooksContext, { Provider } from "../Context/books";
+import { useContext } from "react";
 
 function BookList({ books, onDelete, onEdit }) {
+  const { count, incrementCount } = useContext(BooksContext);
   const renderedBook = books.map((book) => {
     return (
       <BookShow onDelete={onDelete} book={book} key={book.id} onEdit={onEdit} />
     );
   });
-  return <div className="book-list">{renderedBook}</div>;
+  return (
+    <div className="book-list">
+      {count}
+      <button onClick={incrementCount}>Click</button>
+      {renderedBook}
+    </div>
+  );
 }
 
 export default BookList;
