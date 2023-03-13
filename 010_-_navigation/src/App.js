b93f3b4d9525e4bc1c6238bc62e6b47be348b724
Sahar <sahar.solt@gmail.com>
2023-03-13 17:09:51 +0200

    New project

---
+++ 010_-_navigation/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Button from "./Components/Button";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Button>Click me!</Button>
+      </div>
+      <div>
+        <Button>Hide Adds!</Button>
+      </div>
+      <div>
+        <Button>Update!</Button>
+      </div>
+      <div>
+        <Button>Register!</Button>
+      </div>
+      <div>
+        <Button>Follow!</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
