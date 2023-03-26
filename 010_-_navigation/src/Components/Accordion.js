f472355ddf728e9db4a337a534ba99f283d869c3
Sahar <sahar.solt@gmail.com>
2023-03-26 19:14:43 +0300

    Conditional rendering and some styling

--- 010_-_navigation/src/Components/Accordion.js
+++ 010_-_navigation/src/Components/Accordion.js
@@ -1,13 +1,51 @@
+import { useState } from "react";
+import { AiFillCaretDown, AiFillCaretLeft } from "react-icons/ai";
+
 function Accordion({ items }) {
-  const renderdItem = items.map((item) => {
+  const [expandedIndex, setExpandedIndex] = useState(-1);
+
+  const handleClick = (nextIndex) => {
+    setExpandedIndex((currentExpandedIndex) => {
+      if (currentExpandedIndex === nextIndex) {
+        return -1;
+      } else {
+        return nextIndex;
+      }
+    });
+  };
+  //   if (nextIndex === expandedIndex) {
+  //     setExpandedIndex(-1);
+  //   } else {
+  //     setExpandedIndex(nextIndex);
+  //   }
+  // };
+
+  const renderdItem = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+
+    const icon = (
+      <span className="text-2xl">
+        {isExpanded ? <AiFillCaretDown /> : <AiFillCaretLeft />}
+      </span>
+    );
+
+    // const content = isExpanded && <div>{item.content}</div>;
     return (
       <div key={item.id}>
-        <div>{item.label}</div>
-        <div>{item.content}</div>
+        <div
+          className="flex justify-between bg-gray-50 p-3 border-b items-center cursor-pointer"
+          onClick={() => handleClick(index)}
+        >
+          {item.label}
+          {icon}
+        </div>
+        <div>
+          {isExpanded && <div className="border-b p-5">{item.content}</div>}
+        </div>
       </div>
     );
   });
-  return <div> {renderdItem}</div>;
+  return <div className="border-x border-t rounded"> {renderdItem}</div>;
 }
 
 export default Accordion;
