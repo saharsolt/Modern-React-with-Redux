60c51ec20da0df6372125647bf9d648bd7cf204b
Sahar <sahar.solt@gmail.com>
2023-03-29 17:37:51 +0300

    Navigation context

--- 010_-_navigation/src/App.js
+++ 010_-_navigation/src/App.js
@@ -1,21 +1,5 @@
-import React from "react";
-import Dropdown from "./components/Dropdown";
-import { useState } from "react";
-
 function App() {
-  const [selection, setSelection] = useState(null);
-
-  const handleSelect = (option) => {
-    setSelection(option);
-  };
-  const options = [
-    { label: "Red", value: "red" },
-    { label: "Green", value: "green" },
-    { label: "Yellow", value: "yellow" },
-  ];
-  return (
-    <Dropdown options={options} onChange={handleSelect} value={selection} />
-  );
+  return <div>Hello!</div>;
 }
 
 export default App;
--- 010_-_navigation/src/index.js
+++ 010_-_navigation/src/index.js
@@ -2,8 +2,13 @@ import "./index.css";
 import React from "react";
 import reactDOM from "react-dom/client";
 import App from "./App";
+import { NavigationProvider } from "./context/Navigation";
 
 const el = document.getElementById("root");
 const root = reactDOM.createRoot(el);
 
-root.render(<App />);
+root.render(
+  <NavigationProvider>
+    <App />
+  </NavigationProvider>
+);
