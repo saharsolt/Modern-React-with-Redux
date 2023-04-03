460840eed1ab195c1973da81bb6403eb6eaba4fc
Sahar <sahar.solt@gmail.com>
2023-04-03 18:14:51 +0300

    customizing header cells

--- 010_-_navigation/src/pages/TablePage.js
+++ 010_-_navigation/src/pages/TablePage.js
@@ -3,9 +3,9 @@ import Table from "../components/Table";
 function TablePage() {
   const data = [
     { name: "Orange", color: "bg-orange-500", score: 5 },
-    { name: "Apple", color: "bg-red-800", score: 4 },
+    { name: "Apple", color: "bg-red-800", score: 2 },
     { name: "Banana", color: "bg-yellow-200", score: 3 },
-    { name: "Lime", color: "bg-green-300", score: 2 },
+    { name: "Lime", color: "bg-green-300", score: 1 },
   ];
 
   const config = [
@@ -14,7 +14,11 @@ function TablePage() {
       label: "Color",
       render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div>,
     },
-    { label: "Score", render: (fruit) => fruit.score },
+    {
+      label: "Score",
+      render: (fruit) => fruit.score,
+      header: () => <th className="bg-red-300">Score</th>,
+    },
   ];
 
   const keyFn = (fruit) => {
