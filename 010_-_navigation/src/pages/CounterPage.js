5b313276ea006e38c4df330d2af89533bcba1571
Sahar <sahar.solt@gmail.com>
2023-04-07 19:43:36 +0300

    Update state with reducer

--- 010_-_navigation/src/pages/CounterPage.js
+++ 010_-_navigation/src/pages/CounterPage.js
@@ -1,31 +1,106 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import Button from "../components/Button";
 import Panel from "../components/Panel";
 
+const INCREMENT_COUNT = "increment-count";
+const DECREMENT_COUNT = "decrement-count";
+const SET_VALUE_TO_ADD = "change-value";
+const ADD_VALUE_TO_COUNT = "add-value-to-count";
+function reducer(state, action) {
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case DECREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    case SET_VALUE_TO_ADD:
+      return {
+        ...state,
+        valueToAdd: action.payload,
+      };
+    case ADD_VALUE_TO_COUNT:
+      return {
+        ...state,
+        count: state.count + state.valueToAdd,
+        valueToAdd: 0,
+      };
+    default:
+      return state;
+  }
+
+  // if (action.type === INCREMENT_COUNT) {
+  //   return {
+  //     ...state,
+  //     count: state.count + 1,
+  //   };
+  // }
+  // if (action.type === DECREMENT_COUNT) {
+  //   return {
+  //     ...state,
+  //     count: state.count - 1,
+  //   };
+  // }
+  // if (action.type === SET_VALUE_TO_ADD) {
+  //   return {
+  //     ...state,
+  //     valueToAdd: action.payload,
+  //   };
+  // }
+  // if (action.type === ADD_VALUE_TO_COUNT) {
+  //   return {
+  //     ...state,
+  //     count: state.count + state.valueToAdd,
+  //   };
+  // }
+  // return state;
+}
+
 function CounterPage({ initalCount }) {
-  const [count, setCount] = useState(initalCount);
-  const [valueToAdd, setValueToAdd] = useState(0);
+  // const [count, setCount] = useState(initalCount);
+  // const [valueToAdd, setValueToAdd] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {
+    count: initalCount,
+    valueToAdd: 0,
+  });
 
   const increment = () => {
-    setCount(count + 1);
+    // setCount(count + 1);
+    dispatch({
+      type: INCREMENT_COUNT,
+    });
   };
 
   const decrement = () => {
-    setCount(count - 1);
+    // setCount(count - 1);
+    dispatch({
+      type: DECREMENT_COUNT,
+    });
   };
   const handleChange = (event) => {
     const value = parseInt(event.target.value) || 0;
-    setValueToAdd(value);
+    // setValueToAdd(value);
+    dispatch({
+      type: SET_VALUE_TO_ADD,
+      payload: value,
+    });
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setCount(count + valueToAdd);
-    setValueToAdd(0);
+    // setCount(count + valueToAdd);
+    // setValueToAdd(0);
+    dispatch({
+      type: ADD_VALUE_TO_COUNT,
+    });
   };
   return (
     <Panel className="m-3">
-      <h1 className="text-lg">Count is {count}</h1>
+      <h1 className="text-lg">Count is {state.count}</h1>
       <div className="flex flex-row">
         <Button onClick={increment}>Increment</Button>
         <Button onClick={decrement}>Decrement</Button>
@@ -33,7 +108,7 @@ function CounterPage({ initalCount }) {
       <form onSubmit={handleSubmit}>
         <label>Add a lot</label>
         <input
-          value={valueToAdd || ""}
+          value={state.valueToAdd || ""}
           onChange={handleChange}
           type="number"
           className="p-1 m-3 border border-gray-300 bg-gray-50"
