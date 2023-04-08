8f93b27e6c99b4d3f332bb48e720dd4c7b8585dd
Sahar <sahar.solt@gmail.com>
2023-04-08 13:31:42 +0300

    First project with Redux

---
+++ 011_-_redux/src/store/index.js
@@ -0,0 +1,52 @@
+import { configureStore, createSlice, createAction } from "@reduxjs/toolkit";
+
+const reset = createAction("app/reset");
+//console.log(reset.toString());
+
+const songsSlice = createSlice({
+  name: "song",
+  initialState: [],
+  reducers: {
+    addSong(state, action) {
+      state.push(action.payload);
+    },
+    removeSong(state, action) {
+      const index = state.indexOf(action.payload);
+      state.splice(index, 1);
+    },
+  },
+  extraReducers(builder) {
+    builder.addCase(reset, (state, action) => {
+      return [];
+    });
+  },
+});
+
+const moviesSlice = createSlice({
+  name: "movie",
+  initialState: [],
+  reducers: {
+    addMovie(state, action) {
+      state.push(action.payload);
+    },
+    removeMovie(state, action) {
+      const index = state.indexOf(action.payload);
+      state.splice(index, 1);
+    },
+  },
+  extraReducers(builder) {
+    builder.addCase(reset, (state, action) => {
+      return [];
+    });
+  },
+});
+
+const store = configureStore({
+  reducer: {
+    songs: songsSlice.reducer,
+    movies: moviesSlice.reducer,
+  },
+});
+export { store, reset };
+export const { addSong, removeSong } = songsSlice.actions;
+export const { addMovie, removeMovie } = moviesSlice.actions;
