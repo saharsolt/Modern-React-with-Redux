783634041087d1bf5fc16604e458d064fd3750dc
Sahar <sahar.solt@gmail.com>
2023-04-09 20:29:07 +0300

    useThunk and some features

--- 013_-_media/src/components/UsersList.js
+++ 013_-_media/src/components/UsersList.js
@@ -1,73 +1,55 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
 import Button from "./Button";
 import Skeleton from "./Skeleton";
-
+import { useThunk } from "../hooks/use-thunk";
 function UsersList() {
-  const dispatch = useDispatch();
-
-  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
-  const [loadingUsersError, setLoadingUsersError] = useState(null);
-  const [isCreatingUser, setIsCreatingUser] = useState(false);
-  const [creatingUserError, setCreatingUserError] = useState(null);
+  const [doFetchUsers, isLoadingUsers, loadingUsersError] =
+    useThunk(fetchUsers);
+  const [doAddUsers, isCreatingUser, creatingUserError] = useThunk(addUser);
 
   const { data } = useSelector((state) => {
     return state.users; //{data:[], isLoading:false, error: null}
   });
   useEffect(() => {
-    setIsLoadingUsers(true);
-    dispatch(fetchUsers())
-      .unwrap()
-      .then(() => {
-        //setIsLoadingUsers(false);
-      })
-      .catch((err) => {
-        setLoadingUsersError(err);
-        //setIsLoadingUsers(false);
-      })
-      .finally(() => {
-        setIsLoadingUsers(false);
-      });
-  }, [dispatch]);
+    doFetchUsers();
+  }, [doFetchUsers]);
 
   const handleUserAdd = () => {
-    setIsCreatingUser(true);
-    dispatch(addUser())
-      .unwrap()
-      .catch((err) => setCreatingUserError(err))
-      .finally(() => setIsCreatingUser(false));
+    doAddUsers();
   };
 
-  if (isLoadingUsers) {
-    return <Skeleton times={6} className="h-6 w-full" />;
-  }
+  let content;
 
-  if (loadingUsersError) {
-    return <div>Error of fetching data</div>;
-  }
-  const renderedUsers = data.map((user) => {
-    return (
-      <div key={user.id} className="mb-2 border rounded">
-        <div className="flex items-center p-2 cursor-pointer justify-between">
-          {user.name}
+  if (isLoadingUsers) {
+    content = <Skeleton times={6} className="h-6 w-full" />;
+  } else if (loadingUsersError) {
+    content = <div>Error of fetching data</div>;
+  } else {
+    content = data.map((user) => {
+      return (
+        <div key={user.id} className="mb-2 border rounded">
+          <div className="flex items-center p-2 cursor-pointer justify-between">
+            {user.name}
+          </div>
         </div>
-      </div>
-    );
-  });
+      );
+    });
+  }
 
   return (
     <div>
-      <div className="flex flex-row justify-between m-3">
+      <div className="flex flex-row justify-between m-3 items-center">
         <h1 className="m-2 text-xl">List of users</h1>
-        {isCreatingUser ? (
-          "Loading"
-        ) : (
-          <Button onClick={handleUserAdd}>+ Add user</Button>
-        )}
+
+        <Button loading={isCreatingUser} onClick={handleUserAdd}>
+          + Add user
+        </Button>
+
         {creatingUserError && "User error"}
       </div>
-      {renderedUsers}
+      {content}
     </div>
   );
 }
---
+++ 013_-_media/src/hooks/use-thunk.js
@@ -0,0 +1,22 @@
+import { useDispatch } from "react-redux";
+import { useState, useCallback } from "react";
+
+function useThunk(thunk) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const dispatch = useDispatch();
+
+  const runThunk = useCallback(
+    (arg) => {
+      setIsLoading(true);
+      dispatch(thunk(arg))
+        .unwrap()
+        .catch((err) => setError(err))
+        .finally(() => setIsLoading(false));
+    },
+    [dispatch, thunk]
+  );
+  return [runThunk, isLoading, error];
+}
+
+export { useThunk };
